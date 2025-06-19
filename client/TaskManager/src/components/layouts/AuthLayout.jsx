2060d3e1171ae5d3c971e5db49eeee5f65891a6a
Divyanshu Vishwakarma <sv090999@gmail.com>
2025-06-19 22:49:43 +0530

    ui update in navbar

--- client/TaskManager/src/components/layouts/AuthLayout.jsx
+++ client/TaskManager/src/components/layouts/AuthLayout.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import Logo from '../../assets/logo.png'
+import Logo1 from '../../assets/logo.png'
+import Logo2 from '../../assets/logo_fav.png'
 
 function AuthLayout({ children }) {
   return (
     <div className='flex'>
       <div className='w-screen h-screen md:w-[50vw] px-12 pt-9 pb-12'>
-        <h2 className='text-lg font-medium text-black'>Task Manager</h2>
+        <div className="flex gap-2 items-center">
+          <img src={Logo2} alt="logo" className='w-[70px] h-[50px]'/>
+          <h2 className='text-2xl font-bold font font- text-black'>Task Manager</h2>
+        </div>
         {children}
       </div>
-      <div className='hidden md:flex w-[40vw] h-screen items-center justify-center bg-cover bg-no-repeat bg-center overflow-hidden p-8'><img src={Logo} />
+      <div className='hidden md:flex w-[40vw] h-screen items-center justify-center bg-cover bg-no-repeat bg-center overflow-hidden p-8'><img src={Logo1} />
       </div>
     </div>
   )
